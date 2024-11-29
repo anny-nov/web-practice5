b5c71adbe665a3b3b30560d248198140201c2c66
Anna Novozhilova <anny-nov@ya.ru>
2024-11-30 00:32:47 +0300

    First try

---
+++ precache-manifest.d2c9bd1dd33439ebbd4af212690353f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a397c1a718fe55b0305b98b5613686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a811a762b362b468ef2",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "4a811a762b362b468ef2",
+    "url": "/static/js/main.df53fea6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
